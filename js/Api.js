62fed4b4345666e812a50d7cbe79e769475a96b1
Goldyga <marcingo@toya.net.pl>
2017-11-29 10:35:21 +0100

    update

---
+++ js/Api.js
@@ -0,0 +1,37 @@
+// Ajax SETUP
+// var prefix = "https://cors-anywhere.herokuapp.com/";
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2503',
+  'X-Auth-Token': '44c4014a17abd3cc1daf5a374442c3be'
+};
+
+$.ajaxSetup({
+	headers: myHeaders,
+	// cache: false
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+}); 
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+      var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+  cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+      col.createCard(card);
+    });
+}
+
+  
